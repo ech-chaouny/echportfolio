e0b06f4aa65f35883d33fb79d2c5b79b521943d4
Ismail ech-chaouny <chaouniismail4@gmail.com>
2023-12-28 23:59:30 +0100

    👋

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -15,7 +15,7 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 1)}>
       <Tilt
         options={{ max: 45, scale: 1, speed: 450 }}
         className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
